ab1e9080202d8da20c48edf881846d6076e1a130
josh <joshwambere@gmail.com>
2020-08-24 05:44:17 +0200

    update & delete todo done

--- Controllers/todo.js
+++ Controllers/todo.js
@@ -1,4 +1,5 @@
 const todoModel = require("../models/todo");
+const { deleteOne } = require("../models/todo");
 
 class Todos {
   async addTodo(req, res) {
@@ -14,11 +15,55 @@ class Todos {
       data: todo,
     });
   }
-  async getTodos(req, res) { 
-    const todos=await todoModel.find();
+  //get all todos
+  async getTodos(req, res) {
+    const todos = await todoModel.find();
     res.send(todos);
+  }
 
-    
+  //delete todo
+  async deleteTodo(req, res) {
+    try {
+      const todoFound = await todoModel.findOne({ _id: req.params.id });
+      if (todoFound) {
+        await todoModel.deleteOne({ _id: req.params.id });
+        res.status(402).send({
+          status:404,
+          message:'todo deleted successfuly'
+        })
+      } else {
+        
+        res.status.json({
+          status: 404,
+          message: "todo not available",
+        });
+      }
+    } catch (error) {
+      res.status(404);
+      res.send({ error: "resources unavailable" });
+    }
+  }
+  //update todo
+
+  async updateTodo(req, res){
+    const todoFound=await todoModel.findOne({_id:req.params.id});
+    if(!todoFound){
+      res.status(404).send({
+        status:'Bad',
+        message:"todo not found"
+      })
+    }else{
+      const todo={
+        name:req.body.name,
+        completed:req.body.completed
+      }
+      await todoModel.updateOne({_id:req.params.id}, todo);
+      res.status(200).send({
+        status:'Ok',
+        message:"todo updated",
+        data: todo
+      })
+    }
   }
 }
 
--- routes/todo.js
+++ routes/todo.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.post("/todos/add", todoContr.addTodo);
 router.get("/todos", todoContr.getTodos);
+router.delete('/todo/delete/:id', todoContr.deleteTodo);
+router.put('/todo/update/:id', todoContr.updateTodo);
 console.log(router);
 module.exports = router;
