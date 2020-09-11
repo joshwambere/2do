5a4ca308390bced1dc7a40c0e6e5ea49c9213bd6
josh <joshwambere@gmail.com>
2020-09-11 05:13:42 +0200

    mult-domain request arror fixed

--- routes.js
+++ routes.js
@@ -3,6 +3,9 @@ const Todo = require("./routes/todo");
 const { model } = require("./models/todo");
 const router = Router();
 
-router.use("/api", Todo);
+router.use("/api", Todo,()=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+});
 
 module.exports = router;
